2a00c414b35374fde6f4b68d7208067c502d9e65
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-01-21 14:11:08 +0530

    first test case

--- src/main/java/simpleCli/Main.java
+++ src/main/java/simpleCli/Main.java
@@ -1,7 +1,9 @@
 package simpleCli;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Main {
 	public static void main(String[] args) {
@@ -16,6 +18,8 @@ public class Main {
 		driver.manage().deleteAllCookies();
 		
 		driver.get("https://www.amazon.in/");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("alexa echo dot");
+		driver.findElement(By.id("nav-search-submit-button")).click();
 		
 	}
 
