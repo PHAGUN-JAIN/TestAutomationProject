14d1a912508e3dd45379ea718fadb719a53091e8
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-01-28 15:27:18 +0530

    updates

--- src/main/java/selenium/automatedtest/SecondTestCase.java
+++ src/main/java/selenium/automatedtest/SecondTestCase.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SecondTestCasse {
+public class SecondTestCase {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\omgill\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		System.out.println("lab1");
